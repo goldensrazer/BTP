704ba04fa3edd3c551e40e7caa6c60e184a784e2
Kelvin R <33947194+goldensrazer@users.noreply.github.com>
2019-04-20 14:47:44 -0300

    atualizando linha api key

--- dashboard.php
+++ dashboard.php
@@ -308,7 +308,7 @@ $mare = 1.5; //exemplo do elerta
           }
 
 </script>
- <script src="https://maps.googleapis.com/maps/api/js?key=APIKEY=initMap"
+ <script src="https://maps.googleapis.com/maps/api/js?key=APIKEY&callback=initMap"
     async defer>
 </script>
 
